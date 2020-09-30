8873e4daea7e137896ba7c5499647c51d3ad0a41
Tomasz Ścibiorek <tomasz.scibiorek@exaco.pl>
2020-09-30 13:17:01 +0200

    Initial commit, empty project

---
+++ src/test/java/com/silenceb/nordea/NordeaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silenceb.nordea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordeaTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
